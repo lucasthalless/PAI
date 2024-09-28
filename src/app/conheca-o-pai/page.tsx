86d7fc9fae68202b4a6696286655568737aaa39a
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2024-09-28 00:35:28 -0300

    feature: add conheca o pai page

---
+++ src/app/conheca-o-pai/page.tsx
@@ -0,0 +1,13 @@
+import Banner from "@src/components/Banner/Banner";
+
+export default function ConhecaOPAI() {
+  return (
+    <main>
+      <Banner
+        bgImg="conheca-o-pai-banner.jpg"
+        title="Quando em apuros, é ele que você chama!"
+        subtitle="Se seu carro precisa de ajuda, conte com o PAI."
+      />
+    </main>
+  );
+}
