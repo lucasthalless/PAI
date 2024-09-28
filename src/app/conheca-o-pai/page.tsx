2eeba84cdd204f3cacd4ab40109fb9f565d54be4
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2024-09-28 00:49:44 -0300

    feature: add founders section

--- src/app/conheca-o-pai/page.tsx
+++ src/app/conheca-o-pai/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@src/components/Banner/Banner";
+import FounderCard from '../../components/FounderCard/FounderCard';
 
 export default function ConhecaOPAI() {
   return (
@@ -8,6 +9,32 @@ export default function ConhecaOPAI() {
         title="Quando em apuros, é ele que você chama!"
         subtitle="Se seu carro precisa de ajuda, conte com o PAI."
       />
+      <section className="founders-section">
+        <h2>Fundadores</h2>
+        <div className="separator"></div>
+        <div className="founders-div">
+          <FounderCard
+            name="Carolina Rodgerio"
+            rm="554975"
+            github="carolrodgerio"
+            imageUrl="https://avatars.githubusercontent.com/u/161485752?v=4"
+          />
+
+          <FounderCard
+            name="Enrico D’Amico"
+            rm="557706"
+            github="Enrico-AD"
+            imageUrl="https://avatars.githubusercontent.com/u/74722460?v=4"
+          />
+
+          <FounderCard
+            name="Lucas Santos"
+            rm="558886"
+            github="lucasthalless"
+            imageUrl="https://avatars.githubusercontent.com/u/67612669?v=4"
+          />
+        </div>
+      </section>
     </main>
   );
 }
