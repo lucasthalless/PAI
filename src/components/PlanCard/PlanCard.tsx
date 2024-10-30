dbaa4061fcb9872a2dadc4fdde9704efacd52254
lucasthalless <lucaswtds@gmail.com>
2024-10-30 19:52:31 -0300

    feature: add plan card

---
+++ src/components/PlanCard/PlanCard.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { ArrowRight } from "../svgs/ArrowRight";
+import { Check } from "../svgs/Check";
+import './style.css';
+
+interface PlanCardProps {
+  name: string;
+  price: string;
+  benefit1: string;
+  benefit2: string;
+  benefit3: string;
+  favorite?: boolean;
+}
+
+export function PlanCard({
+  name,
+  price,
+  benefit1,
+  benefit2,
+  benefit3,
+  favorite,
+}: PlanCardProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/checkout");
+  };
+
+  return (
+    <div className="plan-card">
+      {favorite ? <div className="favorite">O predileto!</div> : null}
+      <div className="plan-card-heading">
+        <h4>{name}</h4>
+        <p>
+          {price} <span>/ usuário</span>
+        </p>
+      </div>
+      <div className="plan-card-benefits">
+        <p>
+          <Check /> {benefit1}
+        </p>
+        <p>
+          <Check position={2} /> {benefit2}
+        </p>
+        <p>
+          <Check position={3} /> {benefit3}
+        </p>
+      </div>
+      <button onClick={() => handleClick()}>
+        Quero esse plano <ArrowRight />
+      </button>
+    </div>
+  );
+}
