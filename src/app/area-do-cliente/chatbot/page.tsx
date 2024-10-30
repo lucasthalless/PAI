9e855fb278140dcd7f0578f16747a3a3ad8db370
lucasthalless <lucaswtds@gmail.com>
2024-10-30 20:37:22 -0300

    feature: chatbot page

---
+++ src/app/area-do-cliente/chatbot/page.tsx
@@ -0,0 +1,11 @@
+import { Chatbot } from "@src/components/Chatbot/Chatbot";
+
+export default function Page()  {
+  return (
+    <>
+      <h1>Chatbot PAI</h1>
+      <div className="separator"></div>
+      <Chatbot />
+    </>
+  );
+};
