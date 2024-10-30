4dcef84ab8e6ba6ad7f88bce29b52d367d49210f
lucasthalless <lucaswtds@gmail.com>
2024-10-30 20:37:48 -0300

    feature: meu-cadastro + minha assinatura pages initial structure

---
+++ src/app/area-do-cliente/meu-cadastro/page.tsx
@@ -0,0 +1,8 @@
+export default function Page()  {
+  return (
+    <>
+      <h1>Meu cadastro</h1>
+      <div className="separator"></div>
+    </>
+  );
+};
---
+++ src/app/area-do-cliente/minha-assinatura/page.tsx
@@ -0,0 +1,8 @@
+export default function Page()  {
+  return (
+    <>
+      <h1>Minha assinatura</h1>
+      <div className="separator"></div>
+    </>
+  );
+};
